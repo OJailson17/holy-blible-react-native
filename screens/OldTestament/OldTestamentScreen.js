712ca4df0a23b2e194eff83e181b60391065f698
OJailson17 <JaylsonO17@gmail.com>
2021-08-18 21:22:47 -0300

    old testament pagination added

--- screens/OldTestament/OldTestamentScreen.js
+++ screens/OldTestament/OldTestamentScreen.js
@@ -3,7 +3,7 @@ import { useContext } from "react";
 import { BookList } from "../../components/BookList/BookList";
 import { GlobalContext } from "../../context/GlobalContext";
 
-export const OldTestamentScreen = () => {
+export const OldTestamentScreen = ({ navigation }) => {
   const { books } = useContext(GlobalContext);
   const pentateucoBooks = [];
   const historicosBooks = [];
@@ -15,42 +15,57 @@ export const OldTestamentScreen = () => {
   const oldTestamentBooks = books.filter((book) => book.testament === "VT");
 
   const pentateuco = oldTestamentBooks.map((book) =>
-    book.group === "Pentateuco" ? pentateucoBooks.push(book.name) : null
+    book.group === "Pentateuco"
+      ? pentateucoBooks.push({ name: book.name, abbrev: book.abbrev.pt })
+      : null
   );
 
   const historicos = oldTestamentBooks.map((book) =>
-    book.group === "Históricos" ? historicosBooks.push(book.name) : null
+    book.group === "Históricos"
+      ? historicosBooks.push({ name: book.name, abbrev: book.abbrev.pt })
+      : null
   );
   const poeticos = oldTestamentBooks.map((book) =>
-    book.group === "Poéticos" ? poeticosBooks.push(book.name) : null
+    book.group === "Poéticos"
+      ? poeticosBooks.push({ name: book.name, abbrev: book.abbrev.pt })
+      : null
   );
   const pMaiores = oldTestamentBooks.map((book) =>
-    book.group === "Profetas maiores" ? pMaioresBooks.push(book.name) : null
+    book.group === "Profetas maiores"
+      ? pMaioresBooks.push({ name: book.name, abbrev: book.abbrev.pt })
+      : null
   );
   const pMenores = oldTestamentBooks.map((book) =>
-    book.group === "Profetas menores" ? pMenoresBooks.push(book.name) : null
+    book.group === "Profetas menores"
+      ? pMenoresBooks.push({ name: book.name, abbrev: book.abbrev.pt })
+      : null
   );
 
   const booksData = [
     {
       group: "Pentateuco",
-      data: pentateucoBooks,
+      data: pentateucoBooks.map((el) => el.name),
+      abbrev: pentateucoBooks.map((el) => el.abbrev),
     },
     {
       group: "Históricos",
-      data: historicosBooks,
+      data: historicosBooks.map((el) => el.name),
+      abbrev: historicosBooks.map((el) => el.abbrev),
     },
     {
       group: "Poéticos",
-      data: poeticosBooks,
+      data: poeticosBooks.map((el) => el.name),
+      abbrev: poeticosBooks.map((el) => el.abbrev),
     },
     {
       group: "Profetas Maiores",
-      data: pMaioresBooks,
+      data: pMaioresBooks.map((el) => el.name),
+      abbrev: pMaioresBooks.map((el) => el.abbrev),
     },
     {
       group: "Profetas Menores",
-      data: pMenoresBooks,
+      data: pMenoresBooks.map((el) => el.name),
+      abbrev: pMenoresBooks.map((el) => el.abbrev),
     },
   ];
 
@@ -58,12 +73,13 @@ export const OldTestamentScreen = () => {
     dataList.push({
       title: booksData[i].group,
       data: booksData[i].data,
+      abbrev: booksData[i].abbrev,
     });
   }
 
   return (
     <>
-      <BookList books={dataList} />
+      <BookList books={dataList} navigation={navigation} />
     </>
   );
 };
