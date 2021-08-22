4582aea17a424214904321cd54c122e6dbddabd0
OJailson17 <JaylsonO17@gmail.com>
2021-08-22 00:11:34 -0300

    modal added to bible screen with add favorite function

--- components/Verse/Verse.js
+++ components/Verse/Verse.js
@@ -1,19 +1,74 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { useContext } from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import * as Random from "expo-random";
 import { GlobalContext } from "../../context/GlobalContext";
+import { addFavorite } from "../../helper/addFavorite";
+import { MyModal } from "../Modal/Modal";
 
-export const Verse = ({ verses }) => {
+export const Verse = ({ verses, biblePage }) => {
+  const { bookName, chapter, book } = useContext(GlobalContext);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [addedVerse, setAddedVerse] = useState({});
+
+  const handlePress = (verse) => {
+    setModalVisible(true);
+    const verseObj = {
+      number: String(verse.number),
+      text: verse.text,
+      name: bookName,
+      chapter: chapter,
+      abbrev: book,
+    };
+    setAddedVerse(verseObj);
+  };
+
+  const addFavoriteFunction = () => {
+    setModalVisible(false);
+    addFavorite(addedVerse);
+    alert("Verso adicionado aos favoritos");
+  };
   return (
     <View style={styles.verseWrapper}>
-      {verses.map((verse) => (
-        <View style={styles.verseContainer} key={Random.getRandomBytes(100)}>
-          <Text style={styles.verseNum}>{String(verse.number)}</Text>
-          <Text style={styles.verseText}>{verse.text}</Text>
-        </View>
-      ))}
+      <MyModal
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+        textBtn={"Adicionar"}
+        modalText={"Deseja adicionar aos favoritos?"}
+        handleFunction={addFavoriteFunction}
+      />
+
+      {verses.map((verse) => {
+        if (biblePage) {
+          return (
+            <TouchableOpacity
+              style={styles.verseContainer}
+              key={Random.getRandomBytes(100)}
+              onLongPress={() => handlePress(verse)}
+            >
+              <Text style={styles.verseNum}>{String(verse.number)}</Text>
+              <Text style={styles.verseText}>{verse.text}</Text>
+            </TouchableOpacity>
+          );
+        } else {
+          return (
+            <View
+              style={styles.verseContainer}
+              key={Random.getRandomBytes(100)}
+            >
+              <Text style={styles.verseNum}>{String(verse.number)}</Text>
+              <Text style={styles.verseText}>{verse.text}</Text>
+            </View>
+          );
+        }
+      })}
     </View>
   );
 };
